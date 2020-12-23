888cc5456256fb7151c70c1e3ebe2efdd03f7655
rj2704 <jigarrathod2704@gmail.com>
2020-12-23 21:04:25 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+
+    const url = 'http://api.weatherapi.com/v1/current.json?key=659f7a181f4e4ffb97e112144202112&units=si&q=' + latitude + ',' + longitude
+
+    request({url, json: true}, (error, { body }) => {
+
+        if(error){
+            callback('Unable to connect to weather service!', undefined)
+        }else if(body.error){
+            callback('Unable to find location', undefined)
+        }else{
+            callback(undefined, 'It is currently '+ body.current.temp_f + ' degrees out. There is a ' + body.current.precip_in + '% chance of rain.' )
+        }
+    })
+}
+
+module.exports = forecast
